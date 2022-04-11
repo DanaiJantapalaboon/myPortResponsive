8c43d4d83c597b48b4caf59bda1d905498bcab81
Danai Jantapalaboon <84336025+DanaiJantapalaboon@users.noreply.github.com>
2022-04-11 21:37:59 +0700

    update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const server = express();
+const app = express();
 const port = 3000;
 const path = require('path');
 
-server.use(express.static(path.join(__dirname, '/page/')));
-server.listen(port, () => {})
+app.use(express.static(path.join(__dirname, '/page/')));
+app.listen(port, () => {})
