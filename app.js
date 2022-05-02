36a33cc66897ce9d1960cf3c28a528501f4cb03f
Danai Jantapalaboon <84336025+DanaiJantapalaboon@users.noreply.github.com>
2022-05-02 22:21:39 +0700

    update

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //สร้าง Nodejs Express Server
 const express = require('express')
 const app = express()
-const port = 5000
+const port = process.env.PORT || 5000
 
 const path = require('path');
 app.use(express.static(path.join(__dirname, '/page/')))
