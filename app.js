a2fa4b2d8cffd9996da36937205a850cfd69b84a
Danai Jantapalaboon <84336025+DanaiJantapalaboon@users.noreply.github.com>
2022-05-11 23:07:43 +0700

    update

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const nodemailer = require('nodemailer')
 
 const PORT = process.env.PORT || 5000
 
@@ -13,6 +14,32 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
     console.log(req.body)
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'danai.athlon@gmail.com',
+            pass: 'kaho300691'
+        }
+    })
+
+    const mailOptions = {
+        from: req.body.email,
+        to: 'danai.athlon@gmail.com',
+        subject: `Message from ${req.body.email}`,
+        text: req.body.message
+    }
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if(error) {
+            console.log(error)
+            res.send('error')
+        }
+        else {
+            console.log('Email sent: ' + info.response)
+            res.send('success')
+        }
+    })
 })
 
 app.listen(PORT, () => {
